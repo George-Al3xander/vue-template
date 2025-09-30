3429e54974023ba51d87f404796dc4e76873dd3f
George <george.valuiskyi@gmail.com>
2025-09-30 22:12:01 +0300

    build: init
    Initial build.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+    <section>
+        <h1>Vue Template</h1>
+        <p>
+            Visit
+            <a
+                href="https://github.com/George-Al3xander/vue-template"
+                target="_blank"
+                rel="noopener"
+                >repository</a
+            >
+            to read the documentation
+        </p>
+    </section>
+</template>
+
+<style scoped></style>
